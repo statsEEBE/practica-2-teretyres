e1b050e2dee12a95f83ce8a333a8ec7f1a599da7
teretyres <teresa.martin.alguacil@estudiantat.upc.edu>
2024-10-04 18:38:19 +0200

    p2 marina

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,46 @@
 #Codigo para problema 2
+
+iris
+mis_dades <- iris
+
+y <- mis_dades$Sepal.Length
+y
+
+x <- mis_dades$Petal.Length
+x
+
+plot(x,y)
+
+xbar <- mean(x)
+xbar
+
+ybar <- mean(y)
+ybar
+
+m <- (sum((x-xbar)*(y-ybar)))/(sum((x-xbar)^2)) #derivada mínims quadrats
+m
+
+b <- ybar-m*xbar
+b
+
+m*1.5+b
+
+mod <- lm(y~x) #lm=lineal model
+mod
+
+data.frame(x=x)
+ypredicted <- predict(mod, data.frame(x=x))
+ypredicted
+
+plot(x, y, pch=16, col="red")
+lines(x, ypredicted)
+
+Rsq <- (sum(((ypredicted-ybar)^2)))/(sum(((y-ybar)^2)))
+Rsq #multiple R-squared
+
+summary(mod)
+
+R <- sqrt(Rsq)
+R #coeficiente de correlación
+
+cor.test(x,y)
